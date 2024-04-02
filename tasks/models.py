bede0e6d773dfc7ea64142b3edf50017d050b167
Marvin <marvinsolano83@gmail.com>
2024-04-02 18:44:16 -0500

    Adding new templates and views

--- tasks/models.py
+++ tasks/models.py
@@ -7,9 +7,9 @@ class Task(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    date_conpleted = models.DateTimeField(null=True, blank=True)
+    date_completed = models.DateTimeField(null=True, blank=True)
     important = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.title
+        return self.title + ' | ' + str(self.user.username)
--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
-# Crear y autenticar usuarios
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm # Crear y autenticar usuarios
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import TaskForm
+from .models import Task
 
 
 # Create your views here
@@ -36,7 +37,44 @@ def signup(request):
 
 
 def tasks(request):
-    return render(request, 'tasks.html')
+    tasks = Task.objects.filter(user=request.user, date_completed__isnull=True)
+    return render(request, 'tasks.html', {'tasks': tasks})
+
+def create_task(request):
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+        'form': TaskForm()
+    })
+    else:
+        try:
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()  
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {
+                'form': TaskForm(),
+                'error': 'Bad data, try again.'
+            })
+        
+def task_detail(request, task_id):
+    if request.method == 'GET':
+        task = get_object_or_404(Task, pk=task_id, user=request.user)
+        form = TaskForm(instance=task)
+        return render(request, 'task_detail.html', {'task': task, 'form': form})
+    else:
+        try:
+            task = get_object_or_404(Task, pk=task_id, user=request.user)
+            form = TaskForm(request.POST, instance=task)
+            form.save()
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'task_detail.html', {
+                'task': task,
+                'form': form,
+                'error': 'Bad info, try again.'
+            })
 
 
 def signout(request):
